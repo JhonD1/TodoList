21feab30dc744f07450aa6eba87003c779241720
JD <johnishuman@gmail.com>
2022-11-30 16:56:19 +0800

    update: change initialization of database

--- ruby/sinatra/app.rb
+++ ruby/sinatra/app.rb
@@ -1,18 +1,17 @@
 require 'sinatra'
 require 'sequel'
+DB = Sequel.sqlite("todo.db") #create in memory temporay Database
 
 get '/' do
   erb :index
 end
 
 get '/add' do
-  DB = Sequel.sqlite("todo.db") #create in memory temporay Database
-
   # drops table if it exists 
   DB.drop_table?(:todo)
 
   # create a table called Todo with id, task, and status  as columns
-  DB.create_table :todo do
+  DB.create_table? :todo do
     primary_key :id
     String :task
     String :status
