1d4d2ddeb18c5d00dd2013c8ed49869cef24b228
JD <johnishuman@gmail.com>
2022-11-30 15:01:52 +0800

    setup: sequel call and output

--- ruby/sinatra/app.rb
+++ ruby/sinatra/app.rb
@@ -1,5 +1,24 @@
 require 'sinatra'
+require 'sequel'
 
 get '/' do
+  DB = Sequel.sqlite #create in memory temporay Database
+
+  # drops table if it exists 
+  DB.drop_table?(:todo)
+
+  # create a table called Todo with id, task, and status  as columns
+  DB.create_table :todo do
+    primary_key :id
+    String :task
+    String :status
+  end
+
+  @todo_list = DB[:todo]
+  @todo_list.insert(task: 'a121', status: 'not done')
+  @todo_list.insert(task: 'grocery', status: 'not done')
+  @todo_list.insert(task: 'todo-list in rails', status: 'not done')
+
+  puts @todo_list
   erb :index
 end
