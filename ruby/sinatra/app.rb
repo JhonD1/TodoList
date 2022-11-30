32b52feffe35a714d88536327f4834fd92b0a782
JD <johnishuman@gmail.com>
2022-11-30 20:31:08 +0800

    update: add proper way to delete and add tasks

--- ruby/sinatra/app.rb
+++ ruby/sinatra/app.rb
@@ -1,28 +1,44 @@
 require 'sinatra'
 require 'sequel'
 
-DB = Sequel.connect("sqlite://todo.db") #create in memory temporay Database if it doesn't exist
+DB = Sequel.connect("sqlite://todo.db") #create in memory temporay Database if it doesn't exist else it will just connect to it
 
-# create a table called Todo with id, task, and status  as columns
-DB.create_table? :todo do
-  primary_key :id
-  String :task
-  String :status
+def make_table
+  # create a table called Todo with id, task, and status  as columns
+  DB.create_table? :todo do
+    primary_key :id
+    String :task
+    String :status
+  end
 end
 
+make_table
+
 get '/' do
-  @todo_list = DB[:todo]
-  erb :index
+  if DB[:todo].first 
+    @todo_list = DB[:todo] 
+    erb :index
+  else
+    erb :no_task
+  end
 end
 
 get '/add' do
+  make_table
 
-
-  @todo_list = DB[:todo]
-  @todo_list.insert(task: 'a121', status: 'not done')
-  @todo_list.insert(task: 'grocery', status: 'not done')
-  @todo_list.insert(task: 'todo-list in rails', status: 'not done')
-
-  puts @todo_list
   erb :add
+end
+
+post '/add' do
+  puts params
+  
+  @todo_list = DB[:todo]
+  @todo_list.insert(task: params[:task], status: params[:task_status])
+
+  redirect "/"
+end
+
+get '/delete' do
+  DB.drop_table?(:todo)
+  erb :no_task
 end
