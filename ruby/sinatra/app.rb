3839f1d9ca9458dc2e3295fa859f6505c064db53
JD <johnishuman@gmail.com>
2022-11-30 19:34:48 +0800

    improve showing of data in a table

--- ruby/sinatra/app.rb
+++ ruby/sinatra/app.rb
@@ -1,9 +1,7 @@
 require 'sinatra'
 require 'sequel'
 
-DB = Sequel.sqlite("todo.db") #create in memory temporay Database
-# drops table if it exists 
-DB.drop_table?(:todo)
+DB = Sequel.connect("sqlite://todo.db") #create in memory temporay Database if it doesn't exist
 
 # create a table called Todo with id, task, and status  as columns
 DB.create_table? :todo do
@@ -13,6 +11,7 @@ DB.create_table? :todo do
 end
 
 get '/' do
+  @todo_list = DB[:todo]
   erb :index
 end
 
@@ -25,4 +24,5 @@ get '/add' do
   @todo_list.insert(task: 'todo-list in rails', status: 'not done')
 
   puts @todo_list
+  erb :add
 end
